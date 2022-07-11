21a4e7f217c308832a80b89683e11835a34bdb7f
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-07-11 03:27:04 +0000

    Update gellany_ova2docker.sh

--- gellany_ova2docker.sh
+++ gellany_ova2docker.sh
@@ -5,7 +5,7 @@ sudo apt-get install qemu-utils -y
 
 tar -xvf analysis-image.ova
 qemu-img convert -O raw analysis-image-disk001.vmdk analysis-image-disk001.raw
-parted -s analysis-image-disk001.raw b print
+sudo fdisk -l analysis-image-disk001.raw
 mkdir /mnt/container
 mount -o loop,ro,offset=1045876 analysis-image-disk001.raw /mnt/container
 ls /mnt/container
